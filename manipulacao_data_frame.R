5de7afbc884472dd98622cdbfc0ee137a522a5ac
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-09-22 23:55:25 -0300

    Add files via upload

--- manipulacao_data_frame.R
+++ manipulacao_data_frame.R
@@ -4,29 +4,29 @@
 ### Criando dois data.frames
 
 Notas_turma_1 <- c(9, 7, 5, 8, 10)
-Alunos <- c("Joao", "Maria", "Augusto", "Pedro", "Paula")
+Alunos1 <- c("Joao", "Maria", "Augusto", "Pedro", "Paula")
 
-dados <- data.frame(Notas_turma_1, Alunos)
+dados <- data.frame(Notas_turma_1, Alunos1)
 dados
 
 Notas_turma_2 <- c(7, 4, 9, 8, 6)
-Alunos <- c("Ana", "Gabriel", "Artur", "Valentina", "Vanessa")
+Alunos2 <- c("Ana", "Gabriel", "Artur", "Valentina", "Vanessa")
 
-dados1 <- data.frame(Notas_turma_2, Alunos)
+dados1 <- data.frame(Notas_turma_2, Alunos2)
 dados1
 
 ### Criando nova coluna "Condicao" com dados associados a outras colunas da sua tabela:
 
 # Condição turma 1
 Condicao_turma_1 <- dados$Notas_turma_1 >= '7'
-dados[Condicao_turma_1, "Condiçao T1"] <- "Aprovado"
-Condicao_turma_1 <- dados$Notas_turma_2 < '7'
-dados[Condicao_turma_1, "Condiçao T2"] <- "Reprovado"
-Condicao_turma_2 <- dados1$Notas_turma_1 >= '7'
-dados[Condicao_turma_2, "Condiçao T1"] <- "Aprovado"
-Condicao_turma_2 <- dados1$Notas_turma_2 < '7'
-dados1[Condicao_turma_2, "Condiçao T2"] <- "Reprovado"
+dados[Condicao_turma_1, "Condicao_turma_1"] <- "Aprovado"
+Condicao_turma_1 <- dados$Notas_turma_1 < '7'
+dados[Condicao_turma_1, "Condicao_turma_1"] <- "Reprovado"
 
 dados
+
+# Condição turma 2
+Condicao_turma_2 <- dados1$Notas_turma_2 < '7'
+dados1[Condicao_turma_2, "Condicao_turma_2"] <- "Reprovado"
 
 dados1
